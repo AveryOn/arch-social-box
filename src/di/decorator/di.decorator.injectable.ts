fd34beb6128b0f362a5e6e724077cc4d9c11aecb
AveryOn <lnkonion2@gmail.com>
2026-06-06 20:08:29 +0400

    [2026-06-06-1780762109] init injectable function to di decorators

--- src/di/decorator/di.decorator.injectable.ts
+++ src/di/decorator/di.decorator.injectable.ts
@@ -1,8 +1,13 @@
-import { ClassDecorator } from "~/di/decorator";
+import { ClassDecorator, Function } from "~/di/decorator";
 
 const INJECTABLE_METADATA_KEY = Symbol('di:injectable')
 
 export function Injectable(): ClassDecorator {
-
+  return (target) => {
+    Reflect.defineMetadata(INJECTABLE_METADATA_KEY, true, target)
+  }
 }
 
+export function isInjectable(target: Function): boolean {
+  return Reflect.getMetadata(INJECTABLE_METADATA_KEY, target) === true
+}
